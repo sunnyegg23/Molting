6da243ce9309ad0cd5234c4a5c090c4c81c0bec9
PbCuP0w0 <a020233020233@gmail.com>
2025-05-05 14:36:33 +0800

    style: page changed

--- client/src/components/SelfSpace.js
+++ client/src/components/SelfSpace.js
@@ -10,7 +10,7 @@ return (
     <Navbar />
     <div style={{ padding: '10px' , marginLeft:"15%", backgroundColor:"#282c34",height:"100%", minHeight: '100vh', color:"white",}}>
       <p style={{marginLeft:"2%",color:"#CCC",fontSize:"26px"}}>個人資料庫</p>
- 
+      
     </div>
   </div>
 );
